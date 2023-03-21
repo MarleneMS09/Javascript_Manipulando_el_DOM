0e26a1e9401036f957a85f6752e86768a87da6ea
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-20 18:29:25 -0600

    se agregaron más funciones y el botón se selecciona y des-selecciona

--- script.js
+++ script.js
@@ -8,13 +8,22 @@ const createTask = (evento) => {
     const task=document.createElement('li');
     task.classList.add('card');
     input.value= '';
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-   task.innerHTML = content;
-   list.appendChild(task)
+
+    console.log(checkComplete());
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask= document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `
+
+
+   <i class="fas fa-trash-alt trashIcon icon"></i>`;
+   //task.innerHTML = content;
+   task.appendChild(taskContent);
+   list.appendChild(task);
+
    console.log(content);
 };
 
@@ -24,4 +33,18 @@ console.log(btn);
 btn.addEventListener('click', createTask);
 
 
-
+const checkComplete= () => {
+    const i = document.createElement("i")
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+    i.addEventListener('click', completeTask);
+    return i;
+}
+//Inmediately invoked function expression IIFE
+const completeTask = (event) =>{
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle("completeIcon");
+    element.classList,toggle('far');
+}
